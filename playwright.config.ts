064758c064a8a398b8efbf9de131f8b1dd50eb0c
tormipoiss <158473006+tormipoiss@users.noreply.github.com>
2025-10-29 08:54:10 +0200

    added first playwright test

---
+++ playwright.config.ts
@@ -0,0 +1,10 @@
+﻿import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+    testDir: './tests',
+    use: {
+        baseURL: 'https://www.saucedemo.com/',   // oluline, et page.goto('/') töötaks
+        trace: 'on-first-retry'
+    },
+    projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }]
+});
